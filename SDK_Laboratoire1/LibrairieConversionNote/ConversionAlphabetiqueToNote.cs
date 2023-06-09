dd672b4b1e1f7e28f28eb8b52e769426f3730660
JeremieHF <2030315@grasset.qc.ca>
2023-06-09 16:12:42 -0400

    Ceci est la version la plus récente

---
+++ SDK_Laboratoire1/LibrairieConversionNote/ConversionAlphabetiqueToNote.cs
@@ -0,0 +1,48 @@
+﻿namespace LibrairieConversionNote
+{
+    public class ConversionAlphabetiqueToNote
+
+
+    {
+
+
+        public static void Transformer(string lettresalphabétique)
+        {
+            lettresalphabétique = lettresalphabétique.ToUpper();
+
+            if (lettresalphabétique == "A")
+            {
+                Console.WriteLine("LA");
+            }
+            else if (lettresalphabétique == "B")
+            {
+                Console.WriteLine("SI");
+            }
+            else if (lettresalphabétique == "C")
+            {
+                Console.WriteLine("DO");
+            }
+            else if (lettresalphabétique == "D")
+            {
+                Console.WriteLine("RÉ");
+            }
+            else if (lettresalphabétique == "E")
+            {
+                Console.WriteLine("MI");
+            }
+            else if (lettresalphabétique == "F")
+            {
+                Console.WriteLine("FA");
+            }
+            else if (lettresalphabétique == "G")
+            {
+                Console.WriteLine("SOL");
+            }
+            else
+            {
+                Console.WriteLine("Veuillez entrez une lettre entre A et G");
+            }
+        }
+
+    }
+}
---
+++ SDK_Laboratoire1/SDK_Laboratoire1/Program.cs
@@ -0,0 +1,18 @@
+﻿using LibrairieConversionNote;
+
+string lettrerentrer;
+
+
+
+do
+{
+    Console.WriteLine("Bienvenue, veuillez rentrer une lettre entre A et G qui correspondent à une note.");
+
+
+    lettrerentrer = Console.ReadLine();
+
+    ConversionAlphabetiqueToNote.Transformer(lettrerentrer);
+
+
+
+} while (lettrerentrer != "A" || lettrerentrer != "a" || lettrerentrer != "B" || lettrerentrer != "b" || lettrerentrer != "C" || lettrerentrer != "c" || lettrerentrer != "D" || lettrerentrer != "d" || lettrerentrer != "E" || lettrerentrer != "e" || lettrerentrer != "F" || lettrerentrer != "f" || lettrerentrer != "G" || lettrerentrer != "g");
